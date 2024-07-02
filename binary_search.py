0a316f5cc391be7ffd0ea2afa31463c0f9ae46dc
Agniva <ghoshagniva52@gmamil.com>
2024-07-02 14:46:23 +0530

    adding linear search and binary search

---
+++ binary_search.py
@@ -0,0 +1,22 @@
+def binary_search(list,key):
+    low=0
+    high=len(list)-1
+    while(low<=high):
+        mid=high+low//2
+        if list[mid]==key:
+            return mid
+        elif key>list[mid]:
+            low=mid+1
+        else:
+            high=mid-1
+    return -1
+n=int(input("Enter the size of the list: "))
+list=[]
+for i in range(n):
+    elements=int(input("Enter the  element of list: "))
+    list.append(elements)
+print(list)
+key=int(input("enter the element you want to seacrh:  "))
+
+index=binary_search(list,key)
+print("your element is on ",index)
